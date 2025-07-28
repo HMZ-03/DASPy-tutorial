efb06e8314dfcdb78bcfdccccd4eb48516730e22
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2025-07-28 15:22:44 +0800

    update latexdpf

--- source/conf.py
+++ source/conf.py
@@ -53,3 +53,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'preamble': r'''
+\usepackage{fontspec}
+\usepackage{polyglossia}
+\setmainlanguage{english}
+\setmainfont{Times New Roman}
+''',
+}
